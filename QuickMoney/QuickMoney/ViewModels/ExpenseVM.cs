41247cea687fb6790621e688754ce643eaf14f50
Sebastian Gebert <achondrias@gmail.com>
2018-02-25 15:58:46 +0100

    Setup of the basic view and viewmodel structure.

---
+++ QuickMoney/QuickMoney/ViewModels/ExpenseVM.cs
@@ -0,0 +1,19 @@
+﻿namespace QuickMoney.ViewModels
+{
+    internal class ExpenseVM : ViewModel
+    {
+        private string _category;
+        public string Category
+        {
+            get => _category;
+            set { this.SetField(ref _category, value); }
+        }
+
+        private double _amount = 0.0;
+        public double Amount
+        {
+            get => _amount;
+            set { this.SetField(ref _amount, value); }
+        }
+    }
+}
---
+++ QuickMoney/QuickMoney/ViewModels/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace QuickMoney.ViewModels
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+            field = value;
+            this.OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
